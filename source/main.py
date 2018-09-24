4ddf840fdeba9df4b2dfec83c3a6a5d738e59112
Carlos Hernan Arce <carlosap256@gmail.com>
2018-09-24 14:53:47 +0200

    Removed dead import

--- source/main.py
+++ source/main.py
@@ -1,8 +1,8 @@
 import logging
-import configparser
 import os
 from os import listdir
 from os.path import isfile, join
+from typing import List
 
 from pdf_report import MetadataToPDF
 from report_settings import ReportSettings
@@ -26,11 +26,11 @@ class MainApp:
         self.hashtag = Hashtag(report_settings.min_word_length)
         self.pdf_report = MetadataToPDF(report_settings)
 
-    def _init_config_file(self)->ReportSettings:
+    def _init_config_file(self) -> ReportSettings:
         logger.info("Reading config file 'config.ini'")
         return ReportSettings('config.ini')
 
-    def execute(self):
+    def execute(self) -> None:
         self.hashtag.reset_hashtags()
         self._process_doc_files()
         sorted_hashtags = self.hashtag.get_sorted_hashtags()
@@ -38,20 +38,18 @@ class MainApp:
         self.pdf_report.generate_report(sorted_hashtags)
         logger.info("Done")
 
-    def _process_doc_files(self):
+    def _process_doc_files(self) -> None:
         file_list = self._get_doc_file_paths()
         for file_path in file_list:
             logger.info("Extracting hashtag from file: " + str(file_path))
             self.hashtag.append_to_hashtags(file_path)
 
-    def _get_doc_file_paths(self):
+    def _get_doc_file_paths(self) -> List[str]:
         resources_dir = os.path.join(BASE_DIR, 'resources')
         text_files = [f for f in listdir(resources_dir) if isfile(join(resources_dir, f)) and f.endswith('.txt')]
-        return map(lambda f: join(resources_dir, f), text_files)
+        return list(map(lambda f: join(resources_dir, f), text_files))
 
 
 if __name__ == "__main__":
-
     main = MainApp()
     main.execute()
-
